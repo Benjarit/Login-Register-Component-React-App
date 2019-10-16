ae8c9bdc35e5b01e6da52af6ab989d638b66753e
Benjarit Hotrabhavananda <hotraben@gmail.com>
2019-10-15 22:47:32 -0500

    Add files via upload

---
+++ api/user.js
@@ -0,0 +1,8 @@
+module.exports =  class User {
+    constructor(email, password, firstName, lastName) {
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
